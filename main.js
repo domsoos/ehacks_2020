8ab9bab8ce9ece343222db7dbf20108caf4596db
Falcons-Royale <36275109+Falcons-Royale@users.noreply.github.com>
2020-06-27 16:44:43 -0700

    photo final

--- main.js
+++ main.js
@@ -2,11 +2,8 @@ var video = document.getElementById('video');
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 
-// Get access to the camera!
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    // Not adding `{ audio: true }` since we only want video now
     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-        //video.src = window.URL.createObjectURL(stream);
         video.srcObject = stream;
         video.play();
     });
