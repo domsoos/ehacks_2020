7e2c92fc15b4bbcf3f24b39dcf557882ecf579c1
Falcons-Royale <36275109+Falcons-Royale@users.noreply.github.com>
2020-06-27 16:43:43 -0700

    photo

---
+++ main.js
@@ -0,0 +1,16 @@
+var video = document.getElementById('video');
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
+// Get access to the camera!
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        //video.src = window.URL.createObjectURL(stream);
+        video.srcObject = stream;
+        video.play();
+    });
+}
+document.getElementById("snap").addEventListener("click", function() {
+	context.drawImage(video, 0, 0, 640, 480);
+});
