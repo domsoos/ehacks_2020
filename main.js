19741e63b0940bfdbfb11234f11be63f9881822f
Falcons-Royale <36275109+Falcons-Royale@users.noreply.github.com>
2020-06-28 11:37:00 -0700

    update

--- main.js
+++ main.js
@@ -19,74 +19,24 @@ document.getElementById("snap").addEventListener("click", function() {
     img.onload = function () {
 
       context.drawImage(img,0,0);
+    }
+    (async () => {
+			var MODEL_URL = "https://github.com/domsoos/ehacks_2020/tree/master/tfjs_model"
+	    var model = await tf.loadLayersModel(MODEL_URL);
+			const input = tf.fromPixels(img);
+      console.log(model.summary());
+      const result = model.predict(input);
+      alert(result);
+		});
 
-    // Turn grayscale image into a 2D array
-    async loadLocalImage(img) {
-      return new Promise((res,rej)=>{
-      imageGet(filename, (err, info) => {
-      if(err){
-         rej(err);
-         return;
-      }
-        const image = tf.fromPixels(info.data)
-        console.log(image, '127');
-        res(image);
-      });
-    }}
-
-    // 2D array sent through our model
-    const MODEL_URL = "https://github.com/domsoos/ehacks_2020/tree/master/tfjs_model"
-    const model = await tf.loadLayersModel(MODEL_URL);
-    console.log(model.summary());
-    const input = tf.tensor2d([10.0],[1,1]);
-    // Predict if it ishealthy or not
-    const result = model.predict(input);
-    
-    // alert the results
-    alert(result);
-
-      /*
-        var oc = document.createElement('canvas'),octx = oc.getContext('2d');
-        oc.width = 320;
-        oc.height = 280;
-        canvas.width = oc.width;
-        canvas.height = oc.height;
-        octx.drawImage(img, 0, 0, oc.width, oc.height);
-        octx.drawImage(oc, 0, 0, oc.width, oc.height);
-        context.drawImage(oc, 0, 0, oc.width, oc.height,0, 0, canvas.width, canvas.height);
-        context.clearRect(0, 0, canvas.width, canvas.height);*/
-      }
-      //finalImage = new Image();
-      //finalImage.src = canvas.toDataURL("image/png")
-      //finalImage.onload = function(){
-       // context.drawImage(finalImage,20,20)
-      //}
-
-  /*  var input = [];
-  for(var i = 0; i < data.length; i += 4) {
-    input.push(data[i + 2] / 255);
-  }*/
-
-      context.drawImage(img,0,0);
-    
-
-    var input = [];
-  // for(var i = 0; i < data.length; i += 4) {
-  //   input.push(data[i + 2] / 255);
-  // }
-
-
-  //predict(input);
   }, false);
 
   function gray(imgObj) {
-      //var canvas = document.createElement('canvas');
-      //var context = canvas.getContext('2d');
+
 
       var imgW = imgObj.width;
       var imgH = imgObj.height;
-    //  canvas.width = imgW;
-      //canvas.height = imgH;
+
 
       context.drawImage(video, 0, 0,width,height);
       var imgPixels = context.getImageData(0, 0, imgW+1000, imgH+1000);
