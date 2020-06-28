a831382f3429113a0a5af904b9c2cd49103ee11f
domsoos <63819914+domsoos@users.noreply.github.com>
2020-06-27 22:42:53 -0700

    slight

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ document.getElementById("snap").addEventListener("click", function() {
   var img = new Image();
     img.src=gray(img);
     img.onload = function () {
+      context.drawImage(img,0,0);/*
         var oc = document.createElement('canvas'),octx = oc.getContext('2d');
         oc.width = 320;
         oc.height = 280;
@@ -25,18 +26,18 @@ document.getElementById("snap").addEventListener("click", function() {
         octx.drawImage(img, 0, 0, oc.width, oc.height);
         octx.drawImage(oc, 0, 0, oc.width, oc.height);
         context.drawImage(oc, 0, 0, oc.width, oc.height,0, 0, canvas.width, canvas.height);
-        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.clearRect(0, 0, canvas.width, canvas.height);*/
       }
-      finalImage = new Image();
-      finalImage.src = canvas.toDataURL("image/png")
-      finalImage.onload = function(){
-        context.drawImage(finalImage,20,20)
-      }
+      //finalImage = new Image();
+      //finalImage.src = canvas.toDataURL("image/png")
+      //finalImage.onload = function(){
+       // context.drawImage(finalImage,20,20)
+      //}
 
-    var input = [];
+  /*  var input = [];
   for(var i = 0; i < data.length; i += 4) {
     input.push(data[i + 2] / 255);
-  }
+  }*/
 
   //predict(input);
   }, false);
