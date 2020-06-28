c1c769107625aa54c93f211d3040190f08229040
Falcons-Royale <36275109+Falcons-Royale@users.noreply.github.com>
2020-06-27 20:54:02 -0700

    grayscale

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var video = document.getElementById('video');
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
+// tf.loadLayersModel(‘model/model.json’).then(function(model) {
+//   window.model = model;
+// });
 
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
@@ -10,14 +13,36 @@ if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 }
 document.getElementById("snap").addEventListener("click", function() {
   var img = new Image();
-  img.onload = function() {
-    context.drawImage(video, 0, 0, 28, 28);
-    data = context.getImageData(0, 0, 28, 28).data;
+    img.src=gray(img);
+    //img.onload
     var input = [];
-    for(var i = 0; i < data.length; i += 4) {
-      input.push(data[i + 2] / 255);
-    }
-    //predict(input);
-  };
-  img.src = canvas.toDataURL('image/png');
+  for(var i = 0; i < data.length; i += 4) {
+    input.push(data[i + 2] / 255);
+  }
+
+  //predict(input);
   }, false);
+
+  function gray(imgObj) {
+      //var canvas = document.createElement('canvas');
+      //var context = canvas.getContext('2d');
+
+      var imgW = imgObj.width;
+      var imgH = imgObj.height;
+    //  canvas.width = imgW;
+      //canvas.height = imgH;
+
+      context.drawImage(video, 0, 0,320,280);
+      var imgPixels = context.getImageData(0, 0, imgW+1000, imgH+1000);
+      for(var y = 0; y < imgPixels.height; y++){
+          for(var x = 0; x < imgPixels.width; x++){
+              var i = (y * 4) * imgPixels.width + x * 4;
+              var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+              imgPixels.data[i] = avg;
+              imgPixels.data[i + 1] = avg;
+              imgPixels.data[i + 2] = avg;
+          }
+      }
+      context.putImageData(imgPixels, imgW, imgH);
+      return canvas.toDataURL("image/png");
+  }
